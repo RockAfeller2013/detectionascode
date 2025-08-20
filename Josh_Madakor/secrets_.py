6b8e0e46270c4c830ff0021009ec810308e9bcc2
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-08-20 13:40:10 +1000

    Add files via upload

---
+++ Josh_Madakor/secrets_.py
@@ -0,0 +1,6 @@
+# Install Azure CLI
+# Run: az login
+# 
+API_KEY = "____" # Fund and put your openAI API key here
+LOG_ANALYTICS_WORKSPACE_ID = "____" # after joining the cyber range, put the Log Analytics Workspace ID here
+# https://portal.azure.com/#@lognpacific.com/resource/subscriptions/3c95e63a-895a-4386-991e-edbbf57de5c8/resourceGroups/Cyber-Range-Admin-SOC/providers/Microsoft.OperationalInsights/workspaces/LAW-Cyber-Range/Overview
